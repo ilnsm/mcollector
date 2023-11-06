581a891da4a67cf190326fc3b241d7e155c36b40
Ilya Anisimov <anisimovia@inbox.ru>
2023-11-06 20:49:22 +0500

    [iter6] refactor server with API struct

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,40 +3,28 @@ package server
 import (
 	"fmt"
 	"github.com/ilnsm/mcollector/internal/server/config"
+	"github.com/ilnsm/mcollector/internal/server/transport"
 	"github.com/ilnsm/mcollector/internal/storage"
-	"log"
+	"github.com/rs/zerolog"
 	"net/http"
 )
 
-const htmlTemplate = `
-<!DOCTYPE html>
-<html>
-<head>
+type API struct {
+	cfg     config.Config
+	storage storage.Storager
+	log     zerolog.Logger
+}
 
-	<title>Metric's' Data</title>
-
-</head>
-<body>
-
-	   <h1>Data</h1>
-	   <ul>
-	   {{range $key, $value := .}}
-	       <li>{{ $key }}: {{ $value }}</li>
-	   {{end}}
-	   </ul>
-
-
-</body>
-</html>
-`
-
-func Run(s storage.Storager) error {
-
-	cfg, err := config.New()
-	if err != nil {
-		log.Fatal("Could not get config")
+func New(cfg config.Config, l zerolog.Logger, s storage.Storager) *API {
+	return &API{
+		cfg:     cfg,
+		storage: s,
+		log:     l,
 	}
+}
 
-	fmt.Println("Start server on", cfg.Endpoint)
-	return http.ListenAndServe(cfg.Endpoint, MetrRouter(s))
+func (a *API) Run() error {
+
+	fmt.Println("Start server on", a.cfg.Endpoint)
+	return http.ListenAndServe(a.cfg.Endpoint, transport.MetrRouter(a.storage))
 }
