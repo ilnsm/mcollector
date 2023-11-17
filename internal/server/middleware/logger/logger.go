e02d73da3a585595ed5b6a3faf29462d6b796c59
Ilya Anisimov <anisimovia@inbox.ru>
2023-11-17 15:42:10 +0500

    [iter6] add logger as API struct field

--- internal/server/middleware/logger/logger.go
+++ internal/server/middleware/logger/logger.go
@@ -1,31 +1,33 @@
 package logger
 
 import (
+	"github.com/rs/zerolog"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rs/zerolog/log"
 )
 
-func RequestLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		uri := r.RequestURI
-		method := r.Method
+func RequestLogger(log zerolog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			uri := r.RequestURI
+			method := r.Method
 
-		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
-		defer func() {
-			log.Info().
-				Str("URI", uri).
-				Str("Method", method).
-				Str("Duration", time.Since(start).String()).
-				Int("Bytes", ww.BytesWritten()).
-				Int("Status", ww.Status()).
-				Msg("")
-		}()
+			defer func() {
+				log.Info().
+					Str("URI", uri).
+					Str("Method", method).
+					Str("Duration", time.Since(start).String()).
+					Int("Bytes", ww.BytesWritten()).
+					Int("Status", ww.Status()).
+					Msg("")
+			}()
 
-		next.ServeHTTP(ww, r)
-	})
+			next.ServeHTTP(ww, r)
+		})
+	}
 }
