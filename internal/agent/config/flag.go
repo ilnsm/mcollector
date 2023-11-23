a183ddd4050055921793f94e15259e7f1f52549f
Ilya Anisimov <anisimovia@inbox.ru>
2023-11-23 23:08:21 +0500

    [iter7] refactor agent's config

--- internal/agent/config/flag.go
+++ internal/agent/config/flag.go
@@ -2,11 +2,16 @@ package config
 
 import (
 	"flag"
+	"time"
 )
 
 func ParseFlag(c *Config) {
+	var ri, pi int
 	flag.StringVar(&c.Endpoint, "a", "localhost:8080", "Configure the server's host:port")
-	flag.IntVar(&c.ReportInterval, "r", 10, "Configure the agent's report interval")
-	flag.IntVar(&c.PollInterval, "p", 2, "Configure the agent's poll interval")
+	flag.IntVar(&ri, "r", 10, "Configure the agent's report interval")
+	flag.IntVar(&pi, "p", 2, "Configure the agent's poll interval")
 	flag.Parse()
+
+	c.ReportInterval = time.Duration(ri) * time.Second
+	c.PollInterval = time.Duration(pi) * time.Second
 }
