461b64d383bcb3e64ca302d011a94351c4ccace4
Ilya Anisimov <anisimovia@inbox.ru>
2023-11-24 19:55:42 +0500

    [iter7] refactor agent's config, fix durations field

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -8,20 +8,33 @@ import (
 )
 
 type Config struct {
-	Endpoint       string        `env:"ADDRESS"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
+	Endpoint       string `env:"ADDRESS"`
+	ReportInterval time.Duration
+	PollInterval   time.Duration
+}
+
+type tmpDurations struct {
+	ReportInterval int `env:"REPORT_INTERVAL"`
+	PollInterval   int `env:"POLL_INTERVAL"`
 }
 
 func New() (Config, error) {
+	var tmp tmpDurations
 	var c Config
 	ParseFlag(&c)
 
-	err := env.Parse(&c)
+	err := env.Parse(&tmp)
+	if err != nil {
+		wrapErr := fmt.Errorf("new agent config error: %w", err)
+		return c, wrapErr
+	}
+	err = env.Parse(&c)
 	if err != nil {
 		wrapErr := fmt.Errorf("new agent config error: %w", err)
 		return c, wrapErr
 	}
 
+	c.ReportInterval = time.Duration(tmp.ReportInterval) * time.Second
+	c.PollInterval = time.Duration(tmp.PollInterval) * time.Second
 	return c, nil
 }
