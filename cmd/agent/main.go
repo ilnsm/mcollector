5046a1189bb0894b28b9e460d4fbe830805b4cc9
Ilya Anisimov <anisimovia@inbox.ru>
2024-01-08 18:56:31 +0500

    [iter15] run agent in 2 gorutines

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
 	"github.com/ospiem/mcollector/internal/agent"
-	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	if err := agent.Run(); err != nil {
-		log.Fatal().Err(err).Msg("cannot start agent")
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+
+	agent.Run(ctx, &wg)
+	handleSignals(cancel)
+
+	wg.Wait()
+	fmt.Println("Graceful shutdown complete")
+}
+
+func handleSignals(cancel context.CancelFunc) {
+
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-sigCh
+	fmt.Printf("Recieved signal: %v\n", sig)
+
+	cancel()
 }
