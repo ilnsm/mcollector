87f39ec40742acf1df135f6a500712aae2511273
Ilya Anisimov <ospiemg@gmail.com>
2024-03-05 23:58:48 +0500

    chore: remove old test
    Signed-off-by: Ilya Anisimov <ospiemg@gmail.com>

--- internal/server/transport/transport_test.go
+++ internal/server/transport/transport_test.go
@@ -14,69 +14,6 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-//
-//func TestUpdateTheMetric(t *testing.T) {
-//	// Create a mock API instance with a mock storage
-//	storage := memorystorage.New()
-//	mockAPI := &API{Storage: storage, Log: zerolog.Logger{}, Cfg: config.Config{}}
-//
-//	tests := []struct {
-//		name       string
-//		url        string
-//		method     string
-//		body       string
-//		statusCode int
-//	}{
-//		{
-//			name:       "BadRequestInvalidGaugeValue",
-//			url:        "/update/gauge/myGauge/invalidValue",
-//			method:     "POST",
-//			body:       "",
-//			statusCode: http.StatusBadRequest,
-//		},
-//
-//		// Test Case 4: Bad request (invalid value for counter)
-//		{
-//			name:       "BadRequestInvalidCounterValue",
-//			url:        "/update/counter/myCounter/invalidValue",
-//			method:     "POST",
-//			body:       "",
-//			statusCode: http.StatusBadRequest,
-//		},
-//
-//		// Test Case 5: Not found (invalid metric type)
-//		{
-//			name:       "NotFoundInvalidMetricType",
-//			url:        "/update/unknownType/myMetric/42.0",
-//			method:     "POST",
-//			body:       "",
-//			statusCode: http.StatusBadRequest,
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			// Create a request with the specified URL, method, and body
-//			req, err := http.NewRequest(tt.method, tt.url, nil)
-//			if err != nil {
-//				t.Fatal(err)
-//			}
-//
-//			// Create a mock response recorder
-//			w := httptest.NewRecorder()
-//
-//			// Call the handler function
-//			handler := UpdateTheMetric(mockAPI)
-//			handler(w, req)
-//
-//			// Check the response status code
-//			if w.Code != tt.statusCode {
-//				t.Errorf("Expected status code %d, got %d", tt.statusCode, w.Code)
-//			}
-//		})
-//	}
-//}
-
 func TestGetTheMetric(t *testing.T) {
 	mockCtl := gomock.NewController(t)
 	defer mockCtl.Finish()
