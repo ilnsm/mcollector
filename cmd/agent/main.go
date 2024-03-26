dfa0c0ae77a637ae6351f46cf736c5c981972da1
Ilya Anisimov <ospiemg@gmail.com>
2024-03-26 11:39:19 +0500

    chore: add ldflags
    Signed-off-by: Ilya Anisimov <ospiemg@gmail.com>

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -16,20 +16,11 @@ import (
 
 const timeoutShutdown = 15 * time.Second
 
-var buildVersion string
-var buildDate string
-var buildCommit string
+var buildVersion string = "N/A"
+var buildDate string = "N/A"
+var buildCommit string = "N/A"
 
 func main() {
-	if buildVersion == "" {
-		buildVersion = "N/A"
-	}
-	if buildDate == "" {
-		buildDate = "N/A"
-	}
-	if buildCommit == "" {
-		buildCommit = "N/A"
-	}
 	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n",
 		buildVersion, buildDate, buildCommit)
 
